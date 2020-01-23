044923e03079db47b10589298d1e73df3ac067fe
Mathuragini <mathuthiyasharma@gmail.com>
2020-01-23 14:33:13 +0530

    Updates

---
+++ precache-manifest.4428edd1c947f91abee95077943eb74d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17ea8eb3a63bb46809059fa0d75cfa0",
+    "url": "/TCGQDMS-Prototype/index.html"
+  },
+  {
+    "revision": "541a1b9ca5f7939297bb",
+    "url": "/TCGQDMS-Prototype/static/css/2.c1531fc8.chunk.css"
+  },
+  {
+    "revision": "f6fb92c51a3d06857f20",
+    "url": "/TCGQDMS-Prototype/static/css/main.3add948f.chunk.css"
+  },
+  {
+    "revision": "541a1b9ca5f7939297bb",
+    "url": "/TCGQDMS-Prototype/static/js/2.985606c1.chunk.js"
+  },
+  {
+    "revision": "f6fb92c51a3d06857f20",
+    "url": "/TCGQDMS-Prototype/static/js/main.3c79e646.chunk.js"
+  },
+  {
+    "revision": "42773f98f20e473368fb",
+    "url": "/TCGQDMS-Prototype/static/js/runtime~main.0f031960.js"
+  },
+  {
+    "revision": "5672055719de0a8050b8c8530245f37e",
+    "url": "/TCGQDMS-Prototype/static/media/AIV.56720557.jpg"
+  },
+  {
+    "revision": "5e5ba4d5f6e3aa949a068b5d3cede37d",
+    "url": "/TCGQDMS-Prototype/static/media/Sieve-Analysis.5e5ba4d5.jpg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/TCGQDMS-Prototype/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "454b91a3f1fa83dd8f55f82d62583198",
+    "url": "/TCGQDMS-Prototype/static/media/images1.454b91a3.jpg"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/TCGQDMS-Prototype/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "4e25be1dc9c4f6e04e1df4a90e6d1aeb",
+    "url": "/TCGQDMS-Prototype/static/media/new1.4e25be1d.png"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/TCGQDMS-Prototype/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "38d77552b0353684a208177482d5b6ee",
+    "url": "/TCGQDMS-Prototype/static/media/primeicons.38d77552.svg"
+  },
+  {
+    "revision": "473e2a746d3c151d7dcaa626a7c84c60",
+    "url": "/TCGQDMS-Prototype/static/media/primeicons.473e2a74.ttf"
+  },
+  {
+    "revision": "71bb3d79dcf18b45ae845409e7c2ada3",
+    "url": "/TCGQDMS-Prototype/static/media/primeicons.71bb3d79.woff"
+  },
+  {
+    "revision": "b8eccb1059ea5faaf6d8b7d457ccfd09",
+    "url": "/TCGQDMS-Prototype/static/media/primeicons.b8eccb10.eot"
+  },
+  {
+    "revision": "0994ab9c078d3e100c555b4bf6ad10c6",
+    "url": "/TCGQDMS-Prototype/static/media/side.0994ab9c.jpg"
+  },
+  {
+    "revision": "4e83dd6325e630eeab3ae94fc559862a",
+    "url": "/TCGQDMS-Prototype/static/media/slump-test1.4e83dd63.jpg"
+  },
+  {
+    "revision": "f97808596b478e06149d618822a850b2",
+    "url": "/TCGQDMS-Prototype/static/media/strength1.f9780859.jpg"
+  },
+  {
+    "revision": "029550c60127777da7fb7e7a3107b2f9",
+    "url": "/TCGQDMS-Prototype/static/media/tokyocement1.029550c6.jpg"
+  }
+]);
